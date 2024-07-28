1c1cc10454122cfb4a4bc02dd56c00914e2f51cf
JulianDevelope <julian.sintelostudios@gmail.com>
2024-07-28 23:42:35 +0200

    changed port to 8080

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const bodyParser = require('body-parser');
 // Create Web Server
 
 const app = express();
-const port = 25565;
+const port = 8080;
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
